ba0b3734051b655211dfa1edb41f9de41a88e791
Juan Esteban Mejía Isaza <juan.mejia@mercadolibre.com.co>
2022-01-23 09:32:41 -0500

    Added PruebaMedidorView 80%

--- app/src/main/java/com/example/intrameplast2022/ui/fragment/FragmentMenu1.kt
+++ app/src/main/java/com/example/intrameplast2022/ui/fragment/FragmentMenu1.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.example.intrameplast2022.R
 import com.example.intrameplast2022.databinding.FragmentHomeBinding
 import com.example.intrameplast2022.databinding.FragmentMenu1Binding
 import kotlin.system.exitProcess
@@ -26,7 +30,39 @@ class FragmentMenu1 : Fragment() {
             findNavController().popBackStack() // Return to the preview fragment, in this case, always homeFragment
         }
 
+        binding.btQ1.setOnClickListener {
+            toast("Caudal (1)")
+        }
+        binding.btQ2.setOnClickListener {
+            toast("Caudal (2)")
+        }
+        binding.btPhoto.setOnClickListener {
+            toast("Fotografía")
+        }
+        binding.btReload.setOnClickListener {
+            toast("Recargado")
+        }
+        binding.btSave.setOnClickListener {
+            toast("Registro guardado")
+        }
+        binding.btPrint.setOnClickListener {
+            toast("Imprimir")
+        }
+
+        // Implementing an exposed dropdown menu
+
+        val items = listOf("Material", "Design", "Components", "Android")
+        val adapter = ArrayAdapter(requireContext(), R.layout.list_item, items)
+        (binding.tvCaliber.editText as? AutoCompleteTextView)?.setAdapter(adapter)
+
+        // End implementation
+
         return binding.root
+    }
+
+    private fun toast(text: String) {
+        // To use Toast inside fragment replace this by context
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
     }
 
 }
