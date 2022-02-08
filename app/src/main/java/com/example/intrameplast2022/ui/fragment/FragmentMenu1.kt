b2ca11e27871849cb9486495da488ccb141c94c3
Juan Esteban Mejía Isaza <juan.mejia@mercadolibre.com.co>
2022-02-08 00:48:11 -0500

    Measure methods improved

--- app/src/main/java/com/example/intrameplast2022/ui/fragment/FragmentMenu1.kt
+++ app/src/main/java/com/example/intrameplast2022/ui/fragment/FragmentMenu1.kt
@@ -1,23 +1,17 @@
 package com.example.intrameplast2022.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Toast
-import androidx.core.widget.doAfterTextChanged
 import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.intrameplast2022.R
 import com.example.intrameplast2022.databinding.FragmentMenu1Binding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
-import kotlin.math.roundToLong
 import kotlin.system.exitProcess
 
 class FragmentMenu1 : Fragment() {
@@ -59,7 +53,7 @@ class FragmentMenu1 : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         binding = FragmentMenu1Binding.inflate(inflater, container, false)
         // Back and Exit buttons, always the same in all fragments
@@ -79,7 +73,6 @@ class FragmentMenu1 : Fragment() {
                 binding.tvResult.visibility = View.GONE
             }
         }
-
 
 
 //      Copy to create Q2 measures
@@ -109,7 +102,7 @@ class FragmentMenu1 : Fragment() {
             reloadFields()
             toast("Recargado")
         }
-        binding.btSave.setOnClickListener {
+        binding.btSave.setOnClickListener { // Show logged info
             if (textFieldsCheck()) {
                 toast("Registro guardado")
             } else toast("¡Complete los campos requeridos!")
@@ -141,7 +134,7 @@ class FragmentMenu1 : Fragment() {
     private fun reloadFields() { // This function reload all table fields to generate a new measure
         // Measure basic info
         // -> Implementation required later
-        // Table
+        // Table start
         binding.tiQ1LI0.setText("")
         binding.tiQ2LI0.setText("")
         binding.tiQ1LF0.setText("")
@@ -154,28 +147,51 @@ class FragmentMenu1 : Fragment() {
         binding.tiQ2TEnvironment0.setText("")
         binding.tiQ1WorkPressure0.setText("")
         binding.tiQ2WorkPressure0.setText("")
+        // Table end
     }
 
     private fun measureProcessQ1(): String {
         val aforo = 99.9
-        var resultQ1 = 0.0
-        var q1WPressure = binding.tiQ1WorkPressure0.text.toString().toFloat()
-        var q1Water = binding.tiQ1TWater0.text.toString().toFloat()
-        if (q1WPressure > 0.0 && q1Water > 0.0) {
-            resultQ1 =
-                (aforo + CVI_) * (1 + CETM_ * (q1Water - 20)) * (1 + CETV_ * (20 - q1Water) * (1 + CCA_ * (q1WPressure - PTR_))) // to code review
+        var vCorregQ1 = 0.0
+        with(binding) {
+            val q1WPressure = tiQ1WorkPressure0.text.toString().toDouble()   //  Presión de trabajo
+            val q1Water = tiQ1TWater0.text.toString().toDouble()             //  Temp agua
+            val q1FinalR = tiQ1LI0.text.toString().toDouble()                //  Lectura final
+            val q1InitialR = tiQ1LF0.text.toString().toDouble()              //  Lectura inicial
+            val q1ProofTime = tiQ1Time0.text.toString().toDouble()           //  Tiempo
+            // Measure Q1
+            if (q1WPressure > 0.0 && q1Water > 0.0) {
+                vCorregQ1 =
+                    (aforo + CVI_) * (1 + CETM_ * (q1Water - 20)) * (1 + CETV_ * (20 - q1Water) * (1 + CCA_ * (q1WPressure - PTR_))) // to code review
+            }
+            vCorregQ1 =
+                ((vCorregQ1 * 1000.0).roundToInt() / 1000.0)        // To return double with 3 decimal digits only
+            val realFlowQ1 = (vCorregQ1 / q1ProofTime) * 3600       // Consult functionality
+            val errorQ1 = (((q1FinalR - q1InitialR) - vCorregQ1) / vCorregQ1) * 100
+            return errorQ1.toString()
         }
-        return ((resultQ1 * 1000.0).roundToInt() / 1000.0).toString() // To return double with 3 decimal digits only
     }
 
     private fun measureProcessQ2(): String {
         val aforo = 9.9
-        var resultQ2 = 0.0
-        val q2Water = binding.tiQ2TWater0.text.toString().toDouble()
-        val q2WPressure = binding.tiQ2WorkPressure0.text.toString().toDouble()
-        resultQ2 =
-            (aforo + CVI_) * (1 + CETM_ * (q2Water - 20)) * (1 + CETV_ * (20 - q2Water) * (1 + CCA_ * (q2WPressure - PTR_))) // to code review
-        return ((resultQ2 * 1000.0).roundToInt() / 1000.0).toString() // To return double with 3 decimal digits only
+        var vCorregQ2 = 0.0
+        with(binding) {
+            val q2WPressure = tiQ2WorkPressure0.text.toString().toDouble()  //  Presión de trabajo
+            val q2Water = tiQ2TWater0.text.toString().toDouble()            //  Temp agua
+            val q2FinalR = tiQ2LI0.text.toString().toDouble()               //  Lectura final
+            val q2InitialR = tiQ2LF0.text.toString().toDouble()             //  Lectura inicial
+            val q2ProofTime = tiQ2Time0.text.toString().toDouble()          //  Tiempo
+            // Measure Q2
+            if (q2WPressure > 0.0 && q2Water > 0.0) {
+                vCorregQ2 =
+                    (aforo + CVI_) * (1 + CETM_ * (q2Water - 20)) * (1 + CETV_ * (20 - q2Water) * (1 + CCA_ * (q2WPressure - PTR_))) // to code review
+            }
+            vCorregQ2 =
+                ((vCorregQ2 * 1000.0).roundToInt() / 1000.0)        // To return double with 3 decimal digits only
+            val realFlowQ2 = (vCorregQ2 / q2ProofTime) * 3600       // Consult functionality
+            val errorQ2 = (((q2FinalR - q2InitialR) - vCorregQ2) / vCorregQ2) * 100
+            return errorQ2.toString()
+        }
     }
 
     private fun toast(text: String) {
