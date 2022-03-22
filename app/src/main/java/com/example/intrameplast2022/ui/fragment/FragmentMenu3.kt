3a3d4e5c44c4bc351b1fa45d71e16e07c3f40caf
Juan Esteban Mejía Isaza <juan.mejia@mercadolibre.com.co>
2022-03-22 17:40:44 -0500

    Improvement export generator

--- app/src/main/java/com/example/intrameplast2022/ui/fragment/FragmentMenu3.kt
+++ app/src/main/java/com/example/intrameplast2022/ui/fragment/FragmentMenu3.kt
@@ -1,20 +1,23 @@
 package com.example.intrameplast2022.ui.fragment
 
+import CourseModal
+import android.app.Activity
 import android.content.Intent
-import android.os.Build
+import android.content.Intent.EXTRA_HTML_TEXT
 import android.os.Bundle
+import android.os.Debug
 import android.provider.DocumentsContract
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.contentValuesOf
 import androidx.navigation.fragment.findNavController
 import com.example.intrameplast2022.MainActivity.Companion.courseModalArrayList
 import com.example.intrameplast2022.R
-import com.example.intrameplast2022.databinding.FragmentHomeBinding
 import com.example.intrameplast2022.databinding.FragmentMenu3Binding
 import java.lang.Exception
 import kotlin.system.exitProcess
@@ -38,13 +41,15 @@ class FragmentMenu3 : Fragment() {
 
         // Export button code
         binding.btExport.setOnClickListener {
+
             val intent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
                 addCategory(Intent.CATEGORY_OPENABLE)
-                type = "text/plain"
-                putExtra(Intent.EXTRA_TITLE, getString(R.string.file_name_txt))
+                type = "text/html"
+                putExtra(Intent.EXTRA_TITLE, getString(R.string.file_name_html))
                 putExtra(DocumentsContract.EXTRA_INITIAL_URI, "")
             }
             startActivityForResult(intent, CREATE_FILE)
+
         }
 
         return binding.root
@@ -54,25 +59,85 @@ class FragmentMenu3 : Fragment() {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == CREATE_FILE && resultCode == AppCompatActivity.RESULT_OK) {
             val uri = data!!.data
+            var mainHTML = ""
             try {
+                courseModalArrayList!!.forEach {
+                    mainHTML += reportsConstructor(it)
+                }
+                println(mainHTML)
+
                 val outputStream = context?.contentResolver?.openOutputStream(uri!!)
-                // outputStream?.write("CodeLib File save Demo".toByteArray())
-
-                val files: String = courseModalArrayList!!.toString()
-                outputStream?.write(files.toByteArray())
-
+                outputStream?.write(mainHTML.toByteArray())
                 outputStream?.close()
                 Toast.makeText(context, getString(R.string.god_exportation), Toast.LENGTH_SHORT)
                     .show()
             } catch (e: Exception) {
                 print(e.localizedMessage)
-                Toast.makeText(context, getString(R.string.bad_exportation), Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, getString(R.string.bad_exportation), Toast.LENGTH_SHORT)
+                    .show()
             }
         }
     }
 
-    private fun reportsConstructor(){
 
+    private fun reportsConstructor2(position: Int): String { // create a single HTML file for each record
+        // Basic info
+        val fecha: String = courseModalArrayList!![position].basicInfo[0]
+        val operador: String = courseModalArrayList!![position].basicInfo[1]
+        val usuario: String = courseModalArrayList!![position].basicInfo[2]
+        val direccion: String = courseModalArrayList!![position].basicInfo[3]
+        val marca: String = courseModalArrayList!![position].basicInfo[4]
+        val serial: String = courseModalArrayList!![position].basicInfo[5]
+        val calibre: String = courseModalArrayList!![position].basicInfo[6]
+        val claseM: String = courseModalArrayList!![position].basicInfo[7]
+        val estado: String = courseModalArrayList!![position].basicInfo[8]
+        // Table
+        val table0: String = courseModalArrayList!![position].tableInfo[0]
+        val table1: String = courseModalArrayList!![position].tableInfo[1]
+        val table2: String = courseModalArrayList!![position].tableInfo[2]
+        val table3: String = courseModalArrayList!![position].tableInfo[3]
+        val table4: String = courseModalArrayList!![position].tableInfo[4]
+        val table5: String = courseModalArrayList!![position].tableInfo[5]
+        val table6: String = courseModalArrayList!![position].tableInfo[6]
+        val table7: String = courseModalArrayList!![position].tableInfo[7]
+        val table8: String = courseModalArrayList!![position].tableInfo[8]
+        val table9: String = courseModalArrayList!![position].tableInfo[9]
+        val table10: String = courseModalArrayList!![position].tableInfo[10]
+        val table11: String = courseModalArrayList!![position].tableInfo[11]
+        val table12: String = courseModalArrayList!![position].tableInfo[12]
+        val table13: String = courseModalArrayList!![position].tableInfo[13]
+
+        return "<!DOCTYPE html> <html lang='es'> <head> <meta charset='UTF-8'> <style> div.content { max-width: 500px } </style> </head> <body> <div class='content'> <hr></hr></hr> <h1 style='text-align: center;'>Prueba de medidor</h1> <hr></hr> <p><strong>Fecha:</strong> $fecha</p> <p><strong>Operador:</strong> $operador</p> <p><strong>Usuario:</strong> $usuario</p> <p><strong>Direcci&oacute;n:</strong> $direccion</p> <p>&nbsp;</p> <h1 style='text-align: center;'>Especificaciones</h1> <p><strong>Marca:</strong> $marca</p> <p><strong>Serial:</strong> $serial</p> <p><strong>Calibre:</strong> $calibre</p> <p><strong>Clase metrol&oacute;gica:</strong> $claseM</p> <p><strong>Estado:</strong> $estado</p> <p>&nbsp;</p> <table style='margin-left: auto; margin-right: auto;' border='1px'> <tbody> <tr> <td style='width: 178.336px; text-align: center;'><h4><span style='text-decoration: underline;'><strong>MEDICIONES</strong></span> </h4></td> <td style='width: 26.7031px; text-align: center;'><span style='text-decoration: underline;'><strong>Q2</strong></span></td> <td style='width: 26.7031px; text-align: center;'><span style='text-decoration: underline;'><strong>Q1</strong></span></td> </tr> <tr style='text-align: center;'> <td style='width: 178.336px;'>Lectura inicial <strong>[L]</strong></td> <td style='width: 26.7031px;'>$table0</td> <td style='width: 26.7031px;'>$table1</td> </tr> <tr style='text-align: center;'> <td style='width: 178.336px;'>Lectura final <strong>[L]</strong></td> <td style='width: 26.7031px;'>$table2</td> <td style='width: 26.7031px;'>$table3</td> </tr> <tr style='text-align: center;'> <td style='width: 178.336px;'>Tiempo <strong>[Seg]</strong></td> <td style='width: 26.7031px;'>$table4</td> <td style='width: 26.7031px;'>$table5</td> </tr> <tr style='text-align: center;'> <td style='width: 178.336px;'>Temp agua <strong>[C&ordm;]</strong></td> <td style='width: 26.7031px;'>$table6</td> <td style='width: 26.7031px;'>$table7</td> </tr> <tr style='text-align: center;'> <td style='width: 178.336px;'>Temp ambiente <strong>[C&ordm;]</strong></td> <td style='width: 26.7031px;'>$table8</td> <td style='width: 26.7031px;'>$table9</td> </tr> <tr style='text-align: center;'> <td style='width: 178.336px;'>Presi&oacute;n de trabajo <strong>[Kpa]</strong></td> <td style='width: 26.7031px;'>$table10</td> <td style='width: 26.7031px;'>$table11</td> </tr> <tr style='text-align: center;'> <td style='width: 178.336px;'><p><span style='color: #339966;'><strong>Resultados</strong></span></p></td> <td style='width: 26.7031px;'><p><strong>$table12</strong></p></td> <td style='width: 26.7031px;'><p><strong>$table13</strong></p></td> </tr> </tbody> </table> <p>&nbsp;</p></div> </body> </html>"
+    }
+
+    private fun reportsConstructor(position: CourseModal): String { // create one HTML file with all records
+        // Basic info
+        val fecha: String = position.basicInfo[0]
+        val operador: String = position.basicInfo[1]
+        val usuario: String = position.basicInfo[2]
+        val direccion: String = position.basicInfo[3]
+        val marca: String = position.basicInfo[4]
+        val serial: String = position.basicInfo[5]
+        val calibre: String = position.basicInfo[6]
+        val claseM: String = position.basicInfo[7]
+        val estado: String = position.basicInfo[8]
+        // Table
+        val table0: String = position.tableInfo[0]
+        val table1: String = position.tableInfo[1]
+        val table2: String = position.tableInfo[2]
+        val table3: String = position.tableInfo[3]
+        val table4: String = position.tableInfo[4]
+        val table5: String = position.tableInfo[5]
+        val table6: String = position.tableInfo[6]
+        val table7: String = position.tableInfo[7]
+        val table8: String = position.tableInfo[8]
+        val table9: String = position.tableInfo[9]
+        val table10: String = position.tableInfo[10]
+        val table11: String = position.tableInfo[11]
+        val table12: String = position.tableInfo[12]
+        val table13: String = position.tableInfo[13]
+
+        return "<!DOCTYPE html> <html lang='es'> <head> <meta charset='UTF-8'> <style> div.content { max-width: 500px } </style> </head> <body> <div class='content'> <hr></hr> <h1 style='text-align: center;'><u>Prueba de medidor</u></h1> <hr></hr> <p><strong>Fecha:</strong> $fecha</p> <p><strong>Operador:</strong> $operador</p> <p><strong>Usuario:</strong> $usuario</p> <p><strong>Direcci&oacute;n:</strong> $direccion</p> <p>&nbsp;</p> <h1 style='text-align: center;'>Especificaciones</h1> <p><strong>Marca:</strong> $marca</p> <p><strong>Serial:</strong> $serial</p> <p><strong>Calibre:</strong> $calibre</p> <p><strong>Clase metrol&oacute;gica:</strong> $claseM</p> <p><strong>Estado:</strong> $estado</p> <p>&nbsp;</p> <table style='margin-left: auto; margin-right: auto;' border='1px'> <tbody> <tr> <td style='width: 178.336px; text-align: center;'><h4><span style='text-decoration: underline;'><strong>MEDICIONES</strong></span> </h4></td> <td style='width: 26.7031px; text-align: center;'><span style='text-decoration: underline;'><strong>Q2</strong></span></td> <td style='width: 26.7031px; text-align: center;'><span style='text-decoration: underline;'><strong>Q1</strong></span></td> </tr> <tr style='text-align: center;'> <td style='width: 178.336px;'>Lectura inicial <strong>[L]</strong></td> <td style='width: 26.7031px;'>$table0</td> <td style='width: 26.7031px;'>$table1</td> </tr> <tr style='text-align: center;'> <td style='width: 178.336px;'>Lectura final <strong>[L]</strong></td> <td style='width: 26.7031px;'>$table2</td> <td style='width: 26.7031px;'>$table3</td> </tr> <tr style='text-align: center;'> <td style='width: 178.336px;'>Tiempo <strong>[Seg]</strong></td> <td style='width: 26.7031px;'>$table4</td> <td style='width: 26.7031px;'>$table5</td> </tr> <tr style='text-align: center;'> <td style='width: 178.336px;'>Temp agua <strong>[C&ordm;]</strong></td> <td style='width: 26.7031px;'>$table6</td> <td style='width: 26.7031px;'>$table7</td> </tr> <tr style='text-align: center;'> <td style='width: 178.336px;'>Temp ambiente <strong>[C&ordm;]</strong></td> <td style='width: 26.7031px;'>$table8</td> <td style='width: 26.7031px;'>$table9</td> </tr> <tr style='text-align: center;'> <td style='width: 178.336px;'>Presi&oacute;n de trabajo <strong>[Kpa]</strong></td> <td style='width: 26.7031px;'>$table10</td> <td style='width: 26.7031px;'>$table11</td> </tr> <tr style='text-align: center;'> <td style='width: 178.336px;'><p><span style='color: #339966;'><strong>Resultados</strong></span></p></td> <td style='width: 26.7031px;'><p><strong>$table12</strong></p></td> <td style='width: 26.7031px;'><p><strong>$table13</strong></p></td> </tr> </tbody> </table> <p>&nbsp;</p></div> </body> </html>"
     }
 
 }
